d8c1c241d5f50fd77870aa983c9a2f043315543d
2ndPrince <astonisher88@gmail.com>
2019-10-31 11:35:42 -0400

    Form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,                               // <========== Add this line!
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
